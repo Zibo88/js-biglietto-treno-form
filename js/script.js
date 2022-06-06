8b85c83212d4385ec48f332da8afcdd0eb72c7bd
Zibo88 <vincenzo.tardino1988@gmail.com>
2022-06-06 15:18:28 +0200

    definite le variabili

---
+++ js/script.js
@@ -0,0 +1,42 @@
+//Il programma dovrà mostrare una form da compilare 
+//con cui chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
+//Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio,
+//secondo queste regole:
+//il prezzo del biglietto è definito in base ai km (0.21 € al km)
+//va applicato uno sconto del 20% per i minorenni
+//va applicato uno sconto del 40% per gli over 65.
+//Il recap dei dati e l'output del prezzo finale va stampato in pagina 
+//(formattato con massimo due decimali, per indicare centesimi sul prezzo). 
+
+
+// Dati da chiedere all'utente
+
+const generateButton = document.getElementById('generate-ticket');
+
+generateButton.addEventListener('click', 
+    function() {
+        const userName = document.getElementById('user-name').value;
+        console.log(userName);
+
+        const userKm = parseInt (document.getElementById('user-km').value);
+        console.log(userKm);
+
+        let userAge = document.getElementById('user-age').value;
+        console.log(userAge);
+
+        // prezzo del biglietto di default
+        const ticketPrice = userKm * 0.21;
+
+        // prezzo biglietto scontato
+
+        
+
+    }
+
+
+)
+
+
+
+
+
